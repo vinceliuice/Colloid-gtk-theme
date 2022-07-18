36e472a8c9b148261475a3c48deebda886131757
vinceliuice <vinceliuice@hotmail.com>
2022-07-18 14:35:27 +0800

    Add firefox theme

---
+++ src/other/firefox/configuration/user.js
@@ -0,0 +1,12 @@
+/* user.js
+ * https://github.com/rafaelmardojai/firefox-gnome-theme/
+ */
+
+// Enable customChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Set UI density to normal
+user_pref("browser.uidensity", 0);
+
+// Enable SVG context-propertes
+user_pref("svg.context-properties.content.enabled", true);
