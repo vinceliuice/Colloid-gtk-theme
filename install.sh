9cbd9f45a2a4951a78d49c917f46e3281499261c
vinceliuice <vinceliuice@hotmail.com>
2024-05-01 08:52:55 +0800

    Fixede #158

--- install.sh
+++ install.sh
@@ -465,7 +465,7 @@ gnome_shell_version() {
 
   sed -i "/\widgets/s/40-0/${GS_VERSION}/" "${SRC_DIR}/sass/gnome-shell/_common-temp.scss"
 
-  if [[ "${GS_VERSION}" == '3-28' ]]; then
+  if [[ "${GS_VERSION}" != '40-0' && "${GS_VERSION}" != '42-0' && "${GS_VERSION}" != '44-0' ]]; then
     sed -i "/\extensions/s/40-0/${GS_VERSION}/" "${SRC_DIR}/sass/gnome-shell/_common-temp.scss"
   fi
 }
