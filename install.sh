4edce1099ca5ba1e5162a8dcf5027f294dd01155
vinceliuice <vinceliuice@hotmail.com>
2025-04-12 14:01:06 +0800

    Fixed #227

--- install.sh
+++ install.sh
@@ -40,8 +40,18 @@ if [[ "$(command -v gnome-shell)" ]]; then
 
   SHELL_VERSION="$(gnome-shell --version | cut -d ' ' -f 3 | cut -d . -f -1)"
 
-  if [[ "${SHELL_VERSION:-}" -ge "40" ]]; then
-    GS_VERSION="${SHELL_VERSION}-0"
+  if [[ "${SHELL_VERSION:-}" -ge "48" ]]; then
+    GS_VERSION="48-0"
+  elif [[ "${SHELL_VERSION:-}" -ge "47" ]]; then
+    GS_VERSION="47-0"
+  elif [[ "${SHELL_VERSION:-}" -ge "46" ]]; then
+    GS_VERSION="46-0"
+  elif [[ "${SHELL_VERSION:-}" -ge "44" ]]; then
+    GS_VERSION="44-0"
+  elif [[ "${SHELL_VERSION:-}" -ge "42" ]]; then
+    GS_VERSION="42-0"
+  elif [[ "${SHELL_VERSION:-}" -ge "40" ]]; then
+    GS_VERSION="40-0"
   else
     GS_VERSION="3-28"
   fi
